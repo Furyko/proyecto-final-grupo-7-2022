69dbfd8858567cec1e26aa9b375a8f34481ab45b
Julian Palavecino <samuguayfenix@gmail.com>
2022-08-16 21:01:15 -0300

    Se agregó environ al requirements, se agregó el archivo .env.example, y se agregaron variables de entorno al settings/local.py

--- proyecto_ONG/settings/local.py
+++ proyecto_ONG/settings/local.py
@@ -1,9 +1,13 @@
 from .base import *
+import environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -11,9 +15,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ONG',
-        'USER': 'proyecto_ong',
-        'PASSWORD': 'infoc4g7',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
         'HOST': 'localhost',
         'PORT':'5432'
     }
