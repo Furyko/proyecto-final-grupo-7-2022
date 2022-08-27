88005e0b2ebb0f2479305a0626b59758e86d1384
Lucas Barrios <lucasbarrios29@gmail.com>
2022-08-27 17:52:47 -0300

    Se creo la carpeta media y el formulario para subir archivos

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "Eventos"
 
 urlpatterns = [
-    path('inicio',views.inicio,name='inicio'),
+    path("", views.uploadFile, name = "uploadFile"),
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(
+        settings.MEDIA_URL, 
+        document_root = settings.MEDIA_ROOT
+    )
--- eventos/views.py
+++ eventos/views.py
@@ -1,4 +1,25 @@
+from . import models
 from django.shortcuts import render
+import templates
 
 def inicio(request):
     return render(request,'inicio.html',{})
+
+def uploadFile(request):
+    if request.method == "POST":
+        # Fetching the form data
+        fileTitle = request.POST["fileTitle"]
+        uploadedFile = request.FILES["uploadedFile"]
+
+        # Saving the information in the database
+        document = models.Document(
+            title = fileTitle,
+            uploadedFile = uploadedFile
+        )
+        document.save()
+
+    documents = models.Document.objects.all()
+
+    return render(request, "eventos/upload-file.html", context = {
+        "files": documents
+    })
