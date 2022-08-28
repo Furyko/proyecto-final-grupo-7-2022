a16ff433d9bae5d2150ccc0c792f138bf04f2f05
Pablo Flea <pablitof13@gmail.com>
2022-08-28 19:19:42 -0300

    git

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from eventos.models import Evento
 
+from usuarios.models import Usuario
+
+
+admin.site.register(Usuario)
+admin.site.register(Evento)
 # Register your models here.
--- eventos/forms.py
+++ eventos/forms.py
@@ -1,2 +1,7 @@
 from django import forms
-from .models import eventos
+from .models import Evento
+
+class EventoForm(forms.ModelForm):
+    class Meta:
+        model = Evento
+        fields = ["nombre"]
